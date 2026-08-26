98d8c8e856e4dad58cd675c394696630f8179979
Nasrul Fatah <nasrul@storee.io>
2026-08-27 00:50:58 +0700

    Fix TypeScript error: remove unsupported fields from GoogleAuth credentials

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -8,10 +8,6 @@ const auth = new google.auth.GoogleAuth({
     private_key_id: "d19ca3456225bfbbc6e1fd9f0f781e6654b32eec",
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    client_id: "113529808784312164020",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   },
   scopes: [
     "https://www.googleapis.com/auth/drive.file",
